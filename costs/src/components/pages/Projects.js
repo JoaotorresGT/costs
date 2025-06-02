157444c65183a5e1cdb96eb963cd1877c1cc7e45
João Torres <joaopedro.torres177@gmail.com>
2025-06-02 12:50:16 -0300

    Loading
    Adicionando o loading do projeto

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from '../layout/Container'
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from "../project/ProjectCard"
 
@@ -12,6 +13,7 @@ import styles from './Projects.module.css'
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
 
     const location = useLocation()
@@ -31,6 +33,7 @@ function Projects() {
         .then(data => {
             console.log(data)
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch(err => console.log(err))
     }, [])
@@ -53,7 +56,10 @@ function Projects() {
                         key={project.id}
                     ></ProjectCard>
                 ))}
-
+                {!removeLoading && <Loading></Loading>}
+                {removeLoading && projects.length === 0 (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
