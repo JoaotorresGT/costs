c6e64b19b03a6054d790588eecbcbd9ccb2cbd08
João Torres <joaopedro.torres177@gmail.com>
2025-06-09 19:16:05 -0300

    Edição de dados
    Adicionando a edição de paginas

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,27 +1,28 @@
-import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer'
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <Navbar></Navbar>
+      <Navbar />
       <Routes>
-          <Route path="/" element={<Container customClass="min-height"><Home /></Container>} />\
-          <Route path="/projects" element={<Container customClass="min-height"><Projects /></Container>} />
-          <Route path="/company" element={<Container customClass="min-height"><Company /></Container>} />
-          <Route path="/contact" element={<Container customClass="min-height"><Contact /></Container>} />
-          <Route path="/newproject" element={<Container customClass="min-height"><NewProject /></Container>} />  
+        <Route path="/" element={<Container customClass="min-height"><Home /></Container>} />
+        <Route path="/projects" element={<Container customClass="min-height"><Projects /></Container>} />
+        <Route path="/company" element={<Container customClass="min-height"><Company /></Container>} />
+        <Route path="/contact" element={<Container customClass="min-height"><Contact /></Container>} />
+        <Route path="/newproject" element={<Container customClass="min-height"><NewProject /></Container>} />
+        <Route path="/project/:id" element={<Container customClass="min-height"><Project /></Container>} />
       </Routes>
-
-      <Footer></Footer>
+      <Footer />
     </Router>
   );
 }
